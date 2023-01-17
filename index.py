b6b52c3c6f6109ca505e089818314a30153c3ab2
HusumDev <102243668+HusumDev@users.noreply.github.com>
2023-01-18 00:27:37 +0300

    Create index.py

---
+++ index.py
@@ -0,0 +1,10 @@
+print("İlk sayıyı giriniz:");
+sayi1 = int(input());
+
+
+print("İkinci sayıyı giriniz:");
+sayi2 = int(input());
+
+toplam = sayi1 + sayi2
+
+print(sayi1, "+", sayi2, "=", toplam);
